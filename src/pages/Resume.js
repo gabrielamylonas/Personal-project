07a21312d884922eb7f2c5b925f3b390b101076c
Gabriela Mylonas <115489489+gabrielamylonas@users.noreply.github.com>
2023-12-20 14:42:21 -0300

    Add files via upload

---
+++ src/pages/Resume.js
@@ -0,0 +1,155 @@
+function Resume() {
+  return (
+    <div className="section">
+      <h2 className="subtitle">Gabriela Ferreira Mylonas</h2>
+      <div className="cv">
+        <div>
+          <p>Summary 💡</p>
+          <span>
+            Front-end developer with knowledge in HTML, CSS, JavaScript, Sass,
+            Tailwind, React, Angular, Object Oriented Programming and Functional
+            Programming, as well as responsive and accessible web design, as
+            well as manipulation of the DOM and databases in Firebase, seeking
+            enter the industry and gain experience in the field.<br></br>
+            Bachelor in Biological Sciences from the Federal University of São
+            Carlos, where I was part of the Observatory of the Sorocaba and
+            Médio Tietê River Basin Committee, promoting activities and
+            expanding Civil Society access and participation in issues and
+            decisions involving the Basin.<br></br> I also participated in the
+            Apetê Capuã Agroecology Center, which promotes articulations and
+            discussions on agroecology in Sorocaba and in the region,
+            culminating in a publication on Agriculture and Conservation, where
+            I was able to analyze the perception and socioeconomic relationship
+            of small rural producers, both agroecological and conventional, with
+            the conservation. As a building technician, I had the experience of
+            managing works and developing civil, infrastructure, energy,
+            metro-rail projects, in addition to working on civil budgets for
+            technical and commercial proposals, including using techniques and
+            knowledge obtained at the University for the production of
+            proposals.
+            <br></br>
+            Being communicative and proactive, I always worked to help my team
+            achieve their goals, which made me work with quotes, purchases,
+            projects and document controls.
+          </span>
+        </div>
+        <div>
+          <p>Academic education 📚</p>
+          <ul>
+            <li className="first">Federal University of São Carlos (UFSCar)</li>
+            <li>
+              <li>
+                Bachelor's degree in Biological Sciences, Agroecology and
+                Sustainability at Interface between supervisory companies and
+                customers; Quotations and issuance of purchase orders for
+                inputs. Agriculture · (2013 - 2017)
+              </li>
+            </li>
+            <br></br>
+            <li className="first">Federal Institute of São Paulo - IFSP</li>
+            <li>
+              <li>Building Technician, Civil Construction · (2006 - 2008)</li>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Experience 🗂️</p>
+          <ul>
+            <li className="first">TS Infrastructure</li>
+            <li className="second">Supply Analyst</li>
+            <li className="second">October 2021 - September 2022 (1 year)</li>
+            <li>
+              <li>
+                Preparation of quotations for technical and commercial
+                proposals;
+              </li>
+              <li>Contact with suppliers to establish partnerships;</li>
+              <li>
+                Analysis of lists of equipment and materials provided by
+                engineering;
+              </li>
+              <li>
+                Studies of tax and fiscal benefits involved in purchases and
+                sales;
+              </li>
+              <li>
+                Creation of a database of competitions, new businesses and
+                suppliers.
+              </li>
+            </li>
+          </ul>
+          <ul>
+            <li className="first">TS Infrastructure</li>
+            <li className="second">Construction Technician</li>
+            <li className="second">
+              January 2019 - September 2021 (2 years 9 months)
+            </li>
+            <li>
+              <li>
+                Survey of quantities of civil infrastructure materials and their
+                technical specifications for greater agility in the production
+                of proposals;
+              </li>
+              <li>
+                Analysis of soil movement volumes through Global Mapper in areas
+                of interest for projects;
+              </li>
+              <li>
+                Quantification of civil works for the preparation of proposals;
+              </li>
+              <li>
+                Execution of As-Builts for Line 5 – Lilac of the São Paulo
+                Metro;
+              </li>
+              <li>
+                Control and review of contractors' projects, with direct contact
+                with designers;
+              </li>
+              <li>
+                Preparation of electrical projects for equipment installation
+                and respective lists of materials indicating manufacturers for
+                reference;
+              </li>
+              <li>Control and issuance of engineering documents.</li>
+            </li>
+          </ul>
+          <ul>
+            <li className="first">CTFGA Consortium</li>
+            <li className="second">Autocad Designer</li>
+            <li className="second">May 2018 - January 2019 (9 months)</li>
+            <li>
+              <li>Control of technical documents</li>
+              <li>
+                Preparation of projects and lists of materials with reference
+                from manufacturers for installation of energy equipment, in
+                accordance with technical standards; Preparation of quotations
+                for technical and commercial proposals; Execution of As-Builts
+                for Line 5 – Lilac of the São Paulo Metro; Control of work
+                execution reports;
+              </li>
+              <li>
+                Preparation of electrical projects with guidance from electrical
+                engineers;
+              </li>
+              <li>
+                Issuance of documents via GED and preparation of delivery notes
+                by Docon;
+              </li>
+              <li>Creation of 3D models in AutoCAD.</li>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Languages 🌎</p>
+          <ul>
+            <li>Portuguese (Native)</li>
+            <li>English (B2)</li>
+            <li>Spanish (A1)</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
